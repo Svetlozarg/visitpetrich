8e1f4672329c53c0622da51f4cd066e2dcde4fba
Svetlozar Georgiev <svetlozar.georgiev@evnisoft.com>
2024-03-21 13:25:09 +0200

    Petrich

--- src/components/Layout/Logo.tsx
+++ src/components/Layout/Logo.tsx
@@ -1,23 +1,24 @@
+import { Box } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
 
 const Logo = () => {
   return (
-    <Link href="/">
-      <Image
-        src="https://ik.imagekit.io/obelussoft/VisitPetrich/visitpetrich-logo_fXv09QIPW.png"
-        alt="VisitPetrich Logo"
-        width={250}
-        height={70}
-        style={{
-          width: "100%",
-          maxWidth: "250px",
-          height: "auto",
-          objectFit: "cover",
-          transition: "250ms ease-in-out",
-        }}
-      />
-    </Link>
+    <Box width="100%" maxWidth={{ xs: "150px", sm: "150px", md: "200px" }}>
+      <Link href="/">
+        <Image
+          src="https://ik.imagekit.io/obelussoft/VisitPetrich/visitpetrich-logo_fXv09QIPW.png"
+          alt="VisitPetrich Logo"
+          width={250}
+          height={70}
+          style={{
+            width: "100%",
+            height: "auto",
+            objectFit: "cover",
+          }}
+        />
+      </Link>
+    </Box>
   );
 };
 
